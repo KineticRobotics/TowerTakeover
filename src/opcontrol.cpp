25931d4e31b0c536cadedd060b69f44525665ca1
Aditya Narayanan <aditya.narayanan03@gmail.com>
2019-07-15 00:41:23 -0500

    added basic drive functions

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,31 +1,58 @@
 #include "main.h"
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor dr4b_left(15);
+pros::Motor dr4b_right(16);
+pros::Motor claw(8, true);
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+pros::Motor rightFront(10, true);
+pros::Motor rightBack(20, true);
+pros::Motor leftFront(1);
+pros::Motor leftBack(11);
+
+void dr4bControl(int stateUp, int stateDown, int cond)
+{
+	if(stateUp == 1 && stateDown == 0 && cond == 0) {
+		dr4b_left = -127;
+		dr4b_right = 127;
+	}
+
+	else if(stateUp == 0 && stateDown == 1 && cond == 0){
+		dr4b_left = 127;
+		dr4b_right = -127;
+	}
+
+	else if(stateUp == 1 && stateDown == 0 && cond == 1){
+		dr4b_left = -30;
+		dr4b_right = 30;
+	}
+
+	else if(stateUp == 0 && stateDown == 1 && cond == 1){
+		dr4b_left = 30;
+		dr4b_right = -30;
+	}
+
+	else if(stateUp == 0 && stateDown == 0){
+		dr4b_left = 0;
+		dr4b_right = 0;
+	}
+}
+
+void clawControl(int stateIn){
+	if (stateIn == 1)
+		claw = 80;
+
+	else if (stateIn == 0)
+		claw = 0;
+}
+
+void opcontrol() {
+	while (true) {
+		dr4bControl(master.get_digital(DIGITAL_R1), master.get_digital(DIGITAL_R2), master.get_digital(DIGITAL_L1));
+		clawControl(master.get_digital(DIGITAL_L2));
+		leftFront = master.get_analog(ANALOG_LEFT_Y);
+		leftBack = master.get_analog(ANALOG_LEFT_Y);
+		rightFront = master.get_analog(ANALOG_RIGHT_Y);
+		rightBack = master.get_analog(ANALOG_RIGHT_Y);
 	}
 }
