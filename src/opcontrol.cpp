a176b62fbfc0e7ea3710d90d306266109f029f0e
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-09-12 16:55:48 -0500

    Add files via upload
    this is the okapi first time code 9-12-19

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,15 +1,40 @@
+#include "okapi/api.hpp"
+using namespace okapi;
 #include "main.h"
-
-//#include "motor.h"
 #include "opfunc.h"
 
+okapi:: MotorGroup right({11, 20});
+okapi:: MotorGroup left({1, 10});
+
+const auto WHEEL_DIAMETER = 4.25_in;
+const auto CHASSIS_WIDTH = 13.5_in;
+auto chassis = ChassisControllerFactory::create(
+  left, right,
+  AbstractMotor::gearset::green,
+  {WHEEL_DIAMETER, CHASSIS_WIDTH}
+);
+// Arm related objects
+ ADIButton armLimitSwitch('H');
+ControllerButton armUpButton(ControllerDigital::R1);
+ControllerButton armDownButton(ControllerDigital::R2);
+ControllerButton clawIn(ControllerDigital::L1);
+ControllerButton clawOut(ControllerDigital::L2);
+Motor armMotor = 8_rmtr;
+
 void opcontrol() {
-	while (true) {
-		dr4bControl(master.get_digital(DIGITAL_R1), master.get_digital(DIGITAL_R2), 0);
-		clawControl(master.get_digital(DIGITAL_L2),  master.get_digital(DIGITAL_L1));
-		leftFront = master.get_analog(ANALOG_LEFT_Y);
-		leftBack = master.get_analog(ANALOG_LEFT_Y);
-		rightFront = master.get_analog(ANALOG_RIGHT_Y);
-		rightBack = master.get_analog(ANALOG_RIGHT_Y);
-	}
+	dr4bControl(armUpButton.isPressed(), armDownButton.isPressed(), 0);
+	clawControl(clawIn.isPressed(),  clawOut.isPressed());
+  // Joystick to read analog values for tank or arcade control
+  // Master controller by default
+  Controller masterController;
+
+
+
+  while (true) {
+    // Tank drive with left and right sticks
+    chassis.tank(masterController.getAnalog(ControllerAnalog::leftY),
+               masterController.getAnalog(ControllerAnalog::rightY));
+
+    pros::delay(10);
+  }
 }
