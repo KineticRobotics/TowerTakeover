edbe776d70806cda7a27d8e1c8a826d7676da5ff
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-09-19 16:57:08 -0500

    Update opcontrol.cpp
    took out things into motor.h

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,25 +1,9 @@
-#include "okapi/api.hpp"
-using namespace okapi;
+//#include "okapi/api.hpp"
 #include "main.h"
 #include "opfunc.h"
+//using namespace okapi;
 
-okapi:: MotorGroup right({11, 20});
-okapi:: MotorGroup left({1, 10});
 
-const auto WHEEL_DIAMETER = 4.25_in;
-const auto CHASSIS_WIDTH = 13.5_in;
-auto chassis = ChassisControllerFactory::create(
-  left, right,
-  AbstractMotor::gearset::green,
-  {WHEEL_DIAMETER, CHASSIS_WIDTH}
-);
-// Arm related objects
- ADIButton armLimitSwitch('H');
-ControllerButton armUpButton(ControllerDigital::R1);
-ControllerButton armDownButton(ControllerDigital::R2);
-ControllerButton clawIn(ControllerDigital::L1);
-ControllerButton clawOut(ControllerDigital::L2);
-Motor armMotor = 8_rmtr;
 
 void opcontrol() {
 	dr4bControl(armUpButton.isPressed(), armDownButton.isPressed(), 0);
