9e66d771df720bf0689a2d4b1dd69ae7946854e0
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-09-19 16:57:34 -0500

    Update motor.h
    added okapi

--- include/motor.h
+++ include/motor.h
@@ -1,7 +1,9 @@
 #include "main.h"
-
+#include "okapi/api.hpp"
 #ifndef _PROS_MOTOR_H_
 #define _PROS_MOTOR_H
+
+using namespace okapi;
 
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 pros::Motor dr4b_left(15);
@@ -12,5 +14,30 @@ pros::Motor rightFront(10, true);
 pros::Motor rightBack(20, true);
 pros::Motor leftFront(1);
 pros::Motor leftBack(11);
+
+
+okapi:: MotorGroup right({11_rmtr, 20_rmtr});
+okapi:: MotorGroup left({1, 10});
+
+const auto WHEEL_DIAMETER = 4.25_in;
+const auto CHASSIS_WIDTH = 13.5_in;
+auto chassis = ChassisControllerFactory::create(
+  left, right,
+  AbstractMotor::gearset::green,
+  {WHEEL_DIAMETER, CHASSIS_WIDTH}
+);
+Motor dr4bL = 15;
+Motor dr4bR = 16;
+auto dr4bLe = IntegratedEncoder(15);
+auto dr4bRe = IntegratedEncoder(16);
+
+// Arm related objects
+//ADIButton armLimitSwitch('H');
+ControllerButton armUpButton(ControllerDigital::R1);
+ControllerButton armDownButton(ControllerDigital::R2);
+ControllerButton clawIn(ControllerDigital::L1);
+ControllerButton clawOut(ControllerDigital::L2);
+Motor armMotor = 8_rmtr;
+
 
 #endif
