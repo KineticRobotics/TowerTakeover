692910268ced29bd61eed25536b477e0fd3641a4
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-09-28 15:58:52 -0500

    Update autonomous.cpp

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include "motor copy.h"
+//#include "opfunc.h" for some reason including anything that includes motor.h is a problem? it says it instantiates it twice? otherwise this works if you uncomment this and pidsimple move
 /*
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 pros::Motor dr4b_left(15);
@@ -12,8 +13,7 @@ pros::Motor leftFront(1);
 pros::Motor leftBack(11);
 */
 
+
 void autonomous() {
-
-
-
+    PIDsimple2.moveDistance(1080);
 }
