65932e7f794667d0b2986be0439a3ee6554daae5
vmusser <vmusser@gmail.com>
2019-10-11 14:17:02 -0500

    **Untested**
    Plan for first competition; hoping to test soon

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,19 +1,32 @@
+
 #include "main.h"
-
-/*
-pros::Controller master(pros::E_CONTROLLER_MASTER);
-pros::Motor dr4b_left(15);
-pros::Motor dr4b_right(16);
-pros::Motor claw(8, true);
-
-pros::Motor rightFront(10, true);
-pros::Motor rightBack(20, true);
-pros::Motor leftFront(1);
-pros::Motor leftBack(11);
-*/
+#include "motorcopy.h"
 
 void autonomous() {
+  PIDsimple.moveDistance(24_in);
 
+  /* NOT TESTED; PLAN FOR FIRST GAME
+  dr4bL.moveVelocity(-200); //move dr4b up
+  dr4bR.moveVelocity(200);
+  pros::delay(1000);
 
+  claw2 = 30;  //open claw a little
+
+  dr4bL.moveVelocity(-200); //move dr4b down
+  dr4bR.moveVelocity(200);
+  pros::delay(1000);
+
+  claw2 = 0; //close claw
+
+  PIDsimple2.turnAngle(350); //IDK? Navigate cubes???
+  PIDsimple2.moveDistance(30_in); //move toward goalzone
+
+  claw2 = 40; //score???
+  pros::delay(500);
+  claw2 = 0;
+
+  PIDsimple2.moveDistance(-5_in); //move backwards
+
+ */
 
 }
