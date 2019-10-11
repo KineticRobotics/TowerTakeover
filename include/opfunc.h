474d371daf9dd69a1a9b3e4c10c88410a43e2648
vmusser <vmusser@gmail.com>
2019-10-11 14:20:12 -0500

    Works, but values may be off
    See title; "velocities" have not been fine-tuned yet

--- include/opfunc.h
+++ include/opfunc.h
@@ -7,54 +7,59 @@
 void dr4bControl(int stateUp, int stateDown, int cond)
 {
 	if(stateUp == 1 && stateDown == 0 && cond == 0) {
-		dr4b_left = -127;
-		dr4b_right = 127;
+		dr4bL.moveVelocity(-200);
+		dr4bR.moveVelocity(200);
 	}
 
 	else if(stateUp == 0 && stateDown == 1 && cond == 0){
-		dr4b_left = 127;
-		dr4b_right = -127;
+		dr4bL.moveVelocity(200);
+		dr4bR.moveVelocity(-200);
 	}
 
+/*
 	else if(stateUp == 1 && stateDown == 0 && cond == 1){
-		dr4b_left = -30;
-		dr4b_right = 30;
+		dr4bL.moveVelocity(-30);
+		dr4bR.moveVelocity(30);
 	}
 
 	else if(stateUp == 0 && stateDown == 1 && cond == 1){
-		dr4b_left = 30;
-		dr4b_right = -30;
+		dr4bL.moveVelocity(30);
+		dr4bR.moveVelocity(-30);
+
 	}
 
+	*/
+
 	else if(stateUp == 0 && stateDown == 0){
-		dr4b_left = 0;
-		dr4b_right = 0;
+		dr4bL.moveVelocity(0);
+		dr4bR.moveVelocity(0);
 	}
 }
 
 void clawControl(int stateIn, int stateOut){
 	if (stateIn == 1 && stateOut == 0)
-		claw = 127;
+		claw.moveVelocity(127);
 
 	else if (stateIn == 0 && stateOut == 1)
-		claw = -63;
+		claw.moveVelocity(-63);
 
   else if (stateIn == 0 && stateOut == 0)
-		claw = 0;
+		claw.moveVelocity(0);
 }
 
+/*
 void dr4bAngle(int angle){
 	int point; int power; int scalar;
 	if(angle<point){
 		power = scalar*(angle/point);
-		/*dr4bL.set(power);
-		dr4bR.set(power);*/
+		dr4bL.set(power);
+		dr4bR.set(power);
 		dr4b_left = power;
 		dr4b_right = power;
 	}
 	else{
-		
 	}
 }
+*/
 
 #endif
