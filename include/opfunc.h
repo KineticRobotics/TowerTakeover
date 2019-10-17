f37315cee9725553ad424e438f67a47714255aaf
vmusser <vmusser@gmail.com>
2019-10-17 16:53:32 -0500

    Updated to Arcade

--- include/opfunc.h
+++ include/opfunc.h
@@ -4,43 +4,36 @@
 #ifndef _PROS_OPFUNC_H_
 #define _PROS_OPFUNC_H_
 
-void dr4bControl(int stateUp, int stateDown, int cond)
+void dr4bControl(int y)
 {
-	if(stateUp == 1 && stateDown == 0 && cond == 0) {
-		dr4b_left = -127;
-		dr4b_right = 127;
-	}
-
-	else if(stateUp == 0 && stateDown == 1 && cond == 0){
-		dr4b_left = 127;
-		dr4b_right = -127;
-	}
-
-	else if(stateUp == 1 && stateDown == 0 && cond == 1){
-		dr4b_left = -30;
-		dr4b_right = 30;
-	}
-
-	else if(stateUp == 0 && stateDown == 1 && cond == 1){
-		dr4b_left = 30;
-		dr4b_right = -30;
-	}
-
-	else if(stateUp == 0 && stateDown == 0){
-		dr4b_left = 0;
-		dr4b_right = 0;
-	}
+	dr4bR.moveVelocity(y);
+	dr4bL.moveVelocity(y);
 }
 
 void clawControl(int stateIn, int stateOut){
 	if (stateIn == 1 && stateOut == 0)
-		claw = 127;
+		claw.moveVelocity(127);
 
 	else if (stateIn == 0 && stateOut == 1)
-		claw = -63;
+		claw.moveVelocity(-63);
 
   else if (stateIn == 0 && stateOut == 0)
-		claw = 0;
+		claw.moveVelocity(0);
 }
+
+/*
+void dr4bAngle(int angle){
+	int point; int power; int scalar;
+	if(angle<point){
+		power = scalar*(angle/point);
+		dr4bL.set(power);
+		dr4bR.set(power);
+		dr4b_left = power;
+		dr4b_right = power;
+	}
+	else{
+	}
+}
+*/
 
 #endif
