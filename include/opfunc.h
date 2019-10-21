f80d857f3998645a8519fa58cf9b50626b7bbd0f
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-10-20 21:18:22 -0500

    Update operator control

--- include/opfunc.h
+++ include/opfunc.h
@@ -4,48 +4,23 @@
 #ifndef _PROS_OPFUNC_H_
 #define _PROS_OPFUNC_H_
 
-void dr4bControl(int stateUp, int stateDown, int cond)
+void dr4bControl(int y)
 {
-	if(stateUp == 1 && stateDown == 0 && cond == 0) {
-		dr4bL.moveVelocity(-200);
-		dr4bR.moveVelocity(200);
-	}
-
-	else if(stateUp == 0 && stateDown == 1 && cond == 0){
-		dr4bL.moveVelocity(200);
-		dr4bR.moveVelocity(-200);
-	}
-
-/*
-	else if(stateUp == 1 && stateDown == 0 && cond == 1){
-		dr4bL.moveVelocity(-30);
-		dr4bR.moveVelocity(30);
-	}
-
-	else if(stateUp == 0 && stateDown == 1 && cond == 1){
-		dr4bL.moveVelocity(30);
-		dr4bR.moveVelocity(-30);
-
-	}
-
-	*/
-
-	else if(stateUp == 0 && stateDown == 0){
-		dr4bL.moveVelocity(0);
-		dr4bR.moveVelocity(0);
-	}
+	dr4bR.moveVelocity(y);
+	dr4bL.moveVelocity(y);
 }
 
-void clawControl(int stateIn, int stateOut){
-	if (stateIn == 1 && stateOut == 0)
-		claw.moveVelocity(127);
-
-	else if (stateIn == 0 && stateOut == 1)
-		claw.moveVelocity(-63);
-
-  else if (stateIn == 0 && stateOut == 0)
+void clawControl(int stateOut, int stateHalf, int stateQuarter)
+{
+	if (stateOut == 1 && stateHalf == 0 && stateQuarter == 0)
+		claw.moveVelocity(-127);
+  	else if (stateOut == 0 && stateHalf == 1 && stateQuarter == 0)
+	  	claw.moveVelocity(63);
+	else if (stateOut == 0 && stateHalf == 0 && stateQuarter == 1)
+		claw.moveVelocity(31);
+	else 
 		claw.moveVelocity(0);
-}
+}	
 
 /*
 void dr4bAngle(int angle){
