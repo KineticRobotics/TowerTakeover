ea18cb2f51588c91d1f9b9306976c7e239a0f75b
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-10-20 21:19:32 -0500

    Fixed a few things
    Should have right directions now regardless of whether or not auton was started first.

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -7,20 +7,22 @@ using namespace okapi;
 
 
 void opcontrol() {
+  dr4bL.setReversed(false);
+  right.setReversed(false);
+  dr4bL.setBrakeMode(AbstractMotor::brakeMode::hold);
+  dr4bR.setBrakeMode(AbstractMotor::brakeMode::hold);
+  // Joystick to read analog values for tank or arcade control
+  // Master controller by default
+  Controller masterController;
 
-// Joystick to read analog values for tank or arcade control
-// Master controller by default
-Controller masterController;
-
-
-dr4bL.setBrakeMode(AbstractMotor::brakeMode::hold);
-dr4bR.setBrakeMode(AbstractMotor::brakeMode::hold);
-while (true) {
-  // Tank drive with left and right sticks
-  chassis.tank(masterController.getAnalog(ControllerAnalog::leftY),
-             -masterController.getAnalog(ControllerAnalog::rightY));
-  dr4bControl(armUpButton.isPressed(), armDownButton.isPressed(), 0);
-  clawControl(clawIn.isPressed(), clawOut.isPressed());
-  pros::delay(10);
+  while (true)
+  {
+    // Tank drive with left and right sticks
+    chassis.arcade(masterController.getAnalog(ControllerAnalog::leftX),
+                   masterController.getAnalog(ControllerAnalog::leftY));
+    dr4b.arcade(0, -masterController.getAnalog(ControllerAnalog::rightY));
+    //dr4bControl(armUpButton.isPressed(), armDownButton.isPressed(), 0);
+    clawControl(clawOut.isPressed(), holdHalf.isPressed(), holdQuarter.isPressed());
+    pros::delay(10);
 }
 }
