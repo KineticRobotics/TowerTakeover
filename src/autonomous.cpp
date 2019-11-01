07b2559dc16fdc023a8f77e36c8ea5877e027bc3
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-10-31 21:34:12 -0500

    Added a few things according to what Jim wants

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -2,7 +2,8 @@
 #include "main.h"
 #include "autofunc.h"
 using namespace okapi;
-void autonomous() {
+void autonomous()
+{
   dr4bL2.setReversed(true);
   right2.setReversed(true);
   //dr4bControl(63, 1000);
@@ -10,14 +11,36 @@ void autonomous() {
   pros::lcd::set_text(1, "Exited");
   pros::delay(100);
   dr4bControl(4.0);
-  pros::lcd::set_text(1,"exit func 2");
-  pros::delay(2000);
-  //PID.moveDistance(24_in);
+  pros::lcd::set_text(1, "exit func 2");
+  pros::delay(500);
+
+  dr4bControl(-4.0);
+  claw2.moveVelocity(127);
+  pros::delay(150);
+  claw2.moveVelocity(0);
+  claw2.moveVelocity(-127);
+  pros::delay(150);
+  claw2.moveVelocity(0);
+
+  dr4bControl(4.0);
+  PIDsimple2.moveDistance(24_in);
+
+
+/*
+  //PIDsimple2.moveDistance(24_in*1.106);
+  PIDsimple2.moveDistance(24_in);
+  pros::lcd::set_text(1, "exit PID");
+  pros::delay(5000);
+  PIDsimple2.moveDistance(-12_in);
+  pros::lcd::set_text(1, "exit 2nd PID");
+  */
+  //PIDsimple2.autotune();
+
   //pros::lcd::set_text(1, "exit func 3");
 
   /* NOT TESTED; PLAN FOR FIRST GAME
-  dr4bL.moveVelocity(-200); //move dr4b up
-  dr4bR.moveVelocity(200);
+  dr4bL2.moveVelocity(-200); //move dr4b up
+  dr4bR2.moveVelocity(200);
   pros::delay(1000);
 
   claw2 = 30;  //open claw a little
@@ -38,5 +61,4 @@ void autonomous() {
   PIDsimple2.moveDistance(-5_in); //move backwards
 
  */
-
 }
