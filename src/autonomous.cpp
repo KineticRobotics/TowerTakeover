e6b64174fe1f25e41b027948bfbd705d9a45d0d7
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-11-09 01:22:52 -0600

    Pre Comp update

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -19,24 +19,43 @@ void autonomous()
   claw2.moveVelocity(-127);
   polynomial(10, 70);
   claw2.moveVelocity(0);
-  pros::delay(250); 
-  dr4bControl(28.0);
-  pros::delay(150);
-  polynomial(19, 120);
+  dr4bControl(14.0);
+  pros::delay(400);
+  polynomial(6.0, 70);
+  dr4bControl2(-8.5);
+  pros::delay(400);
   claw2.moveVelocity(-127);
-  dr4bControl2(-28.0);
-  pros::delay(1500);
+  dr4bControl(-6.0);
+  pros::delay(400);
   claw2.moveVelocity(0);
-  pros::delay(100);
-  claw2.moveVelocity(50);
-  polynomialB(32.0, 150);
-  polyTurnReverse(95, 75, 1);
-  polynomial(33.0, 150);
+  pros::delay(75);
+  claw2.moveVelocity(30);
+  pros::delay(75);
+  polynomialB(18, 100);
+  polyTurn(105, 80, 1);
+  polynomial(15.0, 100);
   claw2.moveVelocity(-127);
-  dr4bControl(15.0);
-  pros::delay(800);
-  polynomialB(9.0, 127);
+  polynomialB(10.0, 80);
   claw2.moveVelocity(0);
+  /*polynomial(6, 80);
+  claw2.moveVelocity(0);
+  polynomial(20, 127);
+  dr4bControl(20.0);
+  polynomial(3, 80);
+  claw2.moveVelocity(-127);
+  dr4bControl(-10);
+  claw2.moveVelocity(0);*/
+  //polyTurn(140, 69)
+  //polynomial();
+  //dr4bControl(4.0);
+  //PIDsimple2.moveDistanceAsync(25_in);
+  //polynomial(25.0, 127);
+  //PIDsimple2.turnAngle(90_deg);
+  //polyTurn(180, 127, .95);
+  //polyTurn(90, 100, 1);
+  //pros::lcd::set_text(1, "Exit PID");
+  //dr4bControl(10.0);
+  
 /*
   //PIDsimple2.moveDistance(24_in*1.106);
   PIDsimple2.moveDistance(24_in);
@@ -72,6 +91,4 @@ void autonomous()
   PIDsimple2.moveDistance(-5_in); //move backwards
 
  */
-
-
 }
