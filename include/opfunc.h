d4c6e79cc51dc78602dc6473f9dd9feb029fea60
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-12-01 20:46:00 -0600

    Update 11/30/19
    Added Strafe wheel

--- include/opfunc.h
+++ include/opfunc.h
@@ -14,13 +14,26 @@ void clawControl(int stateOut, int stateHalf, int stateQuarter)
 {
 	if (stateOut == 1 && stateHalf == 0 && stateQuarter == 0)
 		claw.moveVelocity(-127);
-  	else if (stateOut == 0 && stateHalf == 1 && stateQuarter == 0)
-	  	claw.moveVelocity(63);
+	else if (stateOut == 0 && stateHalf == 1 && stateQuarter == 0)
+	  	claw.moveVelocity(127);
 	else if (stateOut == 0 && stateHalf == 0 && stateQuarter == 1)
 		claw.moveVelocity(31);
-	else 
+	else
 		claw.moveVelocity(0);
-}	
+}
+
+void strafeCont(int leftHalf, int leftQuarter, int rightHalf, int rightQuarter) {
+	if(leftHalf == 1 && leftQuarter == 0 && rightHalf == 0 && rightQuarter == 0)
+		strafe.moveVelocity(100);
+	else if(leftHalf == 0 && leftQuarter == 1 && rightHalf == 0 && rightQuarter == 0)
+		strafe.moveVelocity(50);
+	else if(leftHalf == 0 && leftQuarter == 0 && rightHalf == 1 && rightQuarter == 0)
+		strafe.moveVelocity(-100);
+	else if(leftHalf == 0 && leftQuarter == 0 && rightHalf == 0 && rightQuarter == 1)
+			strafe.moveVelocity(-50);
+	else
+		strafe.moveVelocity(0);
+}
 
 /*
 void dr4bAngle(int angle){
