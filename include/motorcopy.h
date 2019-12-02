62f64c37b718abed0f23b9097bd7ab6c1643de87
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-12-01 20:45:22 -0600

    Update 11/30/19
    Added strafe wheel.

--- include/motorcopy.h
+++ include/motorcopy.h
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "okapi/api.hpp"
-#ifndef _PROS_MOTOR_COPY_H_
-#define _PROS_MOTOR_COPY_H
+#ifndef _PROS_MOTOR_H_
+#define _PROS_MOTOR_H
 
 using namespace okapi;
 
@@ -28,43 +28,36 @@ auto rightEF2 = IntegratedEncoder(20);
 auto leftEB2 = IntegratedEncoder(10);
 
 const auto WHEEL_DIAMETER = 4.25_in;
-//const auto WHEEL_DIAMETER = 4_in;
 const auto CHASSIS_WIDTH = 13.5_in;
 auto chassis2 = ChassisControllerFactory::create(
   left2, right2,
   AbstractMotor::gearset::green,
   {WHEEL_DIAMETER, CHASSIS_WIDTH}
 );
+
 Motor dr4bL2 = 15;
 Motor dr4bR2 = 16;
 Motor claw2 = 7;
+Motor strafe2 = 4;
 auto dr4bLe2 = IntegratedEncoder(15);
 auto dr4bRe2 = IntegratedEncoder(16);
 
+auto dr4b2 = ChassisControllerFactory::create(
+  dr4bL2, dr4bR2,
+  AbstractMotor::gearset::red
+);
 
 // Arm related objects
 //ADIButton armLimitSwitch('H');
-ControllerButton armUpButton2(ControllerDigital::R1);
-ControllerButton armDownButton2(ControllerDigital::R2);
-ControllerButton clawIn2(ControllerDigital::L1);
-ControllerButton clawOut2(ControllerDigital::L2);
-Motor armMotor2 = 7_rmtr;
+//ControllerButton armUpButton(ControllerDigital::R1);
+//ControllerButton armDownButton(ControllerDigital::R2);
+//ControllerButton clawOut2(ControllerDigital::L1);
+//ControllerButton holdHalf2(ControllerDigital::R1);
+//ControllerButton holdQuarter2(ControllerDigital::R2);
+//Motor armMotor = 7_rmtr;
 
 //most simple PID initiation (don't know whether to use std and dont know whether to use create or createPTR)
-double ikP = .001;
-double ikI = .0001;
-double ikD = .001;
-auto controller = IterativeControllerFactory::velPID(ikP, ikI, ikD);
-//auto PIDsimple2 = ChassisControllerFactory::create(controller, left2, right2, AbstractMotor::gearset::green,{WHEEL_DIAMETER, CHASSIS_WIDTH});
-auto PIDsimple2 = ChassisControllerFactory::create(
-  left2, right2,
-  IterativePosPIDController::Gains{0.00125, 0, 0.000},
-  IterativePosPIDController::Gains{0.00125, 0.0, 0.000},
-  IterativePosPIDController::Gains{0.00125, 0.0, 0.000},
-  AbstractMotor::gearset::green,
-  {WHEEL_DIAMETER, CHASSIS_WIDTH}
-);
-
+auto PIDsimple2 = ChassisControllerFactory::create(left2, right2, AbstractMotor::gearset::green,{WHEEL_DIAMETER, CHASSIS_WIDTH});
 ADIGyro gyro = ADIGyro('A', .1);
 
 #endif
