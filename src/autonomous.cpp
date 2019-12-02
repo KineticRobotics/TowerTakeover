1435fa1ce33f04bfd2f0f8478ad2fc50a14bfab8
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-12-01 20:47:48 -0600

    Update 11/30/19
    Red Side 6 Cube stack

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,94 +1,73 @@
-
-#include "main.h"
+//#include "main.h"
 #include "autofunc.h"
 using namespace okapi;
 void autonomous()
 {
-  dr4bL2.setBrakeMode(AbstractMotor::brakeMode::hold);  
+	dr4bL2.setBrakeMode(AbstractMotor::brakeMode::hold);
   dr4bR2.setBrakeMode(AbstractMotor::brakeMode::hold);
+  strafe2.setBrakeMode(AbstractMotor::brakeMode::coast);
   dr4bL2.setReversed(true);
   right2.setReversed(true);
-  //dr4bControl(63, 1000);
-  releaseClaw();
-  pros::delay(100);
-  polynomial(3, 50);
-  dr4bControl(10.0);
+  //releaseClaw();
+  //pros::delay(100);
+
+  polynomial(8, 70);
+  claw2.moveVelocity(127);
   pros::delay(300);
-  dr4bControl(-9.5);
+  //claw2.moveVelocity(0);
+  dr4bControlA(28);
+  polynomial(26, 70);
+  dr4bControl2(-5.0);
+  pros::delay(400);
+  claw2.moveVelocity(-127);
   pros::delay(300);
-  claw2.moveVelocity(-127);
-  polynomial(10, 70);
-  claw2.moveVelocity(0);
-  pros::delay(250);
-  dr4bControl(28.0);
-  pros::delay(150);
-  polynomial(19, 120);
-  claw2.moveVelocity(-127);
-  dr4bControl2(-28.0);
-  pros::delay(1500);
-  claw2.moveVelocity(0);
-  pros::delay(100);
-  claw2.moveVelocity(50);
-  polynomialB(32.0, 150);
-  polyTurn(95, 75, 1);
-  polynomial(33.0, 150);
-  claw2.moveVelocity(-127);
-  dr4bControl(15.0);
-  pros::delay(800);
-  polynomialB(9.0, 127);
-  claw2.moveVelocity(0);
-  /*polynomial(6, 80);
-  claw2.moveVelocity(0);
-  polynomial(20, 127);
-  dr4bControl(20.0);
-  polynomial(3, 80);
-  claw2.moveVelocity(-127);
-  dr4bControl(-10);
-  claw2.moveVelocity(0);*/
-  //polyTurn(140, 69)
-  //polynomial();
-  //dr4bControl(4.0);
-  //PIDsimple2.moveDistanceAsync(25_in);
-  //polynomial(25.0, 127);
-  //PIDsimple2.turnAngle(90_deg);
-  //polyTurn(180, 127, .95);
-  //polyTurn(90, 100, 1);
-  //pros::lcd::set_text(1, "Exit PID");
-  //dr4bControl(10.0);
-  
-/*
-  //PIDsimple2.moveDistance(24_in*1.106);
-  PIDsimple2.moveDistance(24_in);
-  pros::lcd::set_text(1, "exit PID");
-  pros::delay(5000);
-  PIDsimple2.moveDistance(-12_in);
-  pros::lcd::set_text(1, "exit 2nd PID");
-  */
-  //PIDsimple2.autotune();
-
-  //pros::lcd::set_text(1, "exit func 3");
-
-  /* NOT TESTED; PLAN FOR FIRST GAME
-  dr4bL2.moveVelocity(-200); //move dr4b up
-  dr4bR2.moveVelocity(200);
+  dr4bControl2(-21.0);
   pros::delay(1000);
-
-  claw2 = 30;  //open claw a little
-
-  dr4bL.moveVelocity(-200); //move dr4b down
-  dr4bR.moveVelocity(200);
+  claw2.moveVelocity(127);
   pros::delay(1000);
-
-  claw2 = 0; //close claw
-
-  PIDsimple2.turnAngle(350); //IDK? Navigate cubes???
-  PIDsimple2.moveDistance(30_in); //move toward goalzone
-
-  claw2 = 40; //score???
+  claw2.moveVelocity(30);
+  polyTurnReverse(120, 127, .98);
+  dr4bControl2(12.0);
   pros::delay(500);
-  claw2 = 0;
+  polynomial(47.0, 200);
+  dr4bControl2(-3.0);
+  pros::delay(300);
+  claw2.moveVelocity(-127);
+  pros::delay(300);
+  polynomialB(20, 150);
+  claw2.moveVelocity(0);
 
-  PIDsimple2.moveDistance(-5_in); //move backwards
 
- */
+
+
+
+  /*
+  //Get first cube
+	claw2.moveVelocity(-127);
+	pros::delay(100);
+	dr4bControlA(6);
+  polynomial(3, 127);
+	claw2.moveVelocity(127);
+	dr4bControlA(-6);
+	claw2.moveVelocity(-127);
+
+	//Get 4 stack
+	dr4bControlA(24);
+	polynomial(23, 100);
+	claw2.moveVelocity(127);
+	pros::delay(50);
+	dr4bControlA(-5);
+	claw2.moveVelocity(-50);
+	dr4bL2.moveVelocity(20);
+	dr4bR2.moveVelocity(20);
+
+	//turn and score
+	polyTurn(135, 127, .98);
+	polynomial(30, 127);
+	dr4bControlA(6);
+	polynomial(3, 127);
+	claw2.moveVelocity(127);
+	pros::delay(100);
+	polynomialB(15, 127);
+  */
 }
